af45892c9311df5251c99453985bb8f7076b8a04
viewsetting <viewsetting@gmail.com>
2021-08-06 08:49:07 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 
 from src.resnet import BaseResnet
+from src.merge import Merge
 from src.trainer import Trainer
 from src.tester import Tester
 from src.preprocess import get_data_loader
@@ -12,7 +13,8 @@ def train_resnet(epoch=1,batch_size=16,optimizer="adam",lr=1e-4,loss_func="BCE",
     model = BaseResnet()
     if optimizer == "adam":
         opt = torch.optim.Adam(model.parameters(),lr = lr,)
-    
+    elif optimizer == 'adadelta':
+        opt = torch.optim.Adadelta(model.parameters(),lr=1.0,rho=0.95)
     else:
         raise NotImplementedError
     
@@ -31,7 +33,7 @@ def train_resnet(epoch=1,batch_size=16,optimizer="adam",lr=1e-4,loss_func="BCE",
                         model_save_path=model_save_path)
     training.train(epoch=epoch)
 
-def test_resnet(model_path='./tmp/epoch_num_4.pth',batch_size=64):
+def test_resnet(model_path='./tmp/epoch_num_4.pth',batch_size=64,csv_path="./results/baseResnet_prob.csv"):
     model = BaseResnet(pretrained=False)
     model.load_state_dict(torch.load(model_path))
     print(model)
@@ -42,7 +44,7 @@ def test_resnet(model_path='./tmp/epoch_num_4.pth',batch_size=64):
     testing = Tester(model=model,dataloader=test_dataloader,device=device)
 
     lst = testing.generate()
-    testing.outputCSV('./results/baseResnet_prob_original.csv',binary=False)
+    testing.outputCSV(csv_path,binary=False)
     print(lst[:30])
 
     cnt = 0
@@ -54,7 +56,62 @@ def test_resnet(model_path='./tmp/epoch_num_4.pth',batch_size=64):
 
     print(cnt)
 
+def train_merge(epoch=1,batch_size=16,optimizer="adam",lr=1e-4,loss_func="BCE",val_ratio=0.2,model_save_path='./tmp/merge/',log_path='./log/merge',freeze=False):
+    model = Merge(pretrained=True,freeze=freeze)
+    if optimizer == "adam":
+        opt = torch.optim.Adam(model.parameters(),lr = lr,)
+    elif optimizer == 'adadelta':
+        opt = torch.optim.Adadelta(model.parameters(),lr=1.0,rho=0.95)
+    else:
+        raise NotImplementedError
+    
+    if loss_func=="BCE":
+        criterion = torch.nn.BCELoss()
+    else:
+        raise NotImplementedError
+
+    lr_sheduler = torch.optim.lr_scheduler.StepLR(optimizer=opt,step_size=5,gamma=0.5)
+
+    #get_dataloader
+    train_dataloader,val_dataloader = get_data_loader(batch_size=batch_size,mode='train',val_ratio=val_ratio,transformList=["resnet","inception","resnext"])
+
+    #initiate Trainer
+    training = Trainer(model,optimizer=opt,criterion=criterion, dataloader=train_dataloader,validate_dataloader=val_dataloader , device=device,
+                        model_save_path=model_save_path,log_path=log_path)
+    training.train(epoch=epoch)
+
+
+def test_merge(model_path='./tmp/merge/epoch_num_2.pth',batch_size=64,csv_path="./results/Merge_prob.csv",binary=False):
+    model = Merge(pretrained=False)
+    model.load_state_dict(torch.load(model_path))
+    print(model)
+
+    #get_dataloader
+    test_dataloader = get_data_loader(batch_size=batch_size,mode='test',dir=TEST_DIR,transformList=["resnet","inception","resnext"])
+
+    testing = Tester(model=model,dataloader=test_dataloader,device=device)
+
+    lst = testing.generate()
+    testing.outputCSV(csv_path,binary=binary)
+    print(lst[:30])
+
+    cnt = 0
+
+    for i,prob in enumerate(lst):
+        if ( (prob >=0.1  and prob <0.5) or (prob <=0.9  and prob>=0.5)) :
+            cnt+=1
+            print(i,'  ',prob)
+
+    print(cnt)
 
 if __name__=="__main__":
     #train_resnet(epoch=5,batch_size=128,model_save_path='./tmp/transform/')
-    test_resnet(model_path='tmp/epoch_num_1.pth')
+    #test_resnet(model_path='tmp/epoch_num_1.pth',csv_path="./results/baseResnet_prob_clipped.csv")
+    
+    #train_merge(epoch=10,batch_size=64,model_save_path='./tmp/merge_fix/',log_path='./logs/merge_fix/')
+    test_merge(model_path='./tmp/merge_fix/epoch_num_3.pth',batch_size=196,csv_path="./results/Merge_fix_prob_clip.csv",binary=False)
+    
+    #freeze pretrained model
+    #train_merge(epoch=10,batch_size=128,model_save_path='./tmp/merge_freeze/',log_path='./logs/merge_freeze/',freeze=True,optimizer='adadelta')
+
+    pass
